1fe516d130203b9b7133cf2c1c63acda4d0a0d6e
Minh Thong <santalucia1992@gmail.com>
2015-08-20 11:24:14 +0700

    First Commit

---
+++ ANA/ANA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ANA
+//
+//  Created by Minh Thong on 8/20/15.
+//  Copyright (c) 2015 Minh Thong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ANA/ANA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ANA
+//
+//  Created by Minh Thong on 8/20/15.
+//  Copyright (c) 2015 Minh Thong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
