419bbe8dc8fac173b4db38325ab4dd5e67435d96
Minh Thong <santalucia1992@gmail.com>
2015-08-20 23:44:46 +0700

    update

---
+++ LAN-Scan-master/LAN Scan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LAN Scan
+//
+//  Created by Mongi Zaidi on 24 February 2014.
+//  Copyright (c) 2014 Smart Touch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LAN-Scan-master/LAN Scan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LAN Scan
+//
+//  Created by Mongi Zaidi on 24 February 2014.
+//  Copyright (c) 2014 Smart Touch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ScanLAN.h"
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, ScanLANDelegate>
+
+@end
