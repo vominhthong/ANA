f9d1126055fb15fa9437a816662a79fa39f2cc02
Minh Thong <santalucia1992@gmail.com>
2015-08-20 21:13:22 +0700

    add storyboard

--- ANA/Controller/MainViewController.h
+++ ANA/Controller/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  ANA
 //
 //  Created by Minh Thong on 8/20/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
